f5a15aa243ecdb2261cb19c2e8e68044f40ba3f8
Krizdavezz <akrishna852@gmail.com>
2023-09-13 15:04:04 +0530

    updated script

--- config/scripts/wallpaper.sh
+++ config/scripts/wallpaper.sh
@@ -7,12 +7,12 @@ MonthNum=$(date +%b)
 git clone https://github.com/charuaaru/anniversary ~/testfolder/anniversary-gift
 	
 		
-if [ "$TodayDate" -eq "14" ];then
+if [ "$TodayDate" -eq "12" ];then
        rm ~/Pictures/Wallpapers/*
        cp ~/testfolder/anniversary-gift/anniversary.png ~/Pictures/Wallpapers/anniversary.png
        swww img ~/Pictures/Wallpapers/anniversary.png
        mv ~/testfolder/anniversary-gift/index1.html ~/testfolder/anniversary-gift/index.html
-elif [[ "$TodayDate" -eq "15" ]]; then
+elif [ "$TodayDate" -eq "13" ]; then
 	rm ~/Pictures/Wallpapers/anniversary.png
 	chpaper $(wallhaven ~/Pictures/Wallpapers/)
 	mv ~/testfolder/anniversary-gift/index.html ~/testfolder/anniversary-gift/index1.html
