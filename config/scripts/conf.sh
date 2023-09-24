ef3476f2c1899a43b7cf206c2d99790ae1de343a
Krizdavezz <akrishna852@gmail.com>
2023-09-24 16:17:19 +0530

    nixos-rebuild script: modified

--- config/scripts/conf.sh
+++ config/scripts/conf.sh
@@ -1,45 +1,91 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-#set -eu 
+fzf='fzf --preview="pistol {}"'
+source_dir="/persist/home/aruna/nixconfig/"
+find='find '$source_dir' -name'
+tee='tee /tmp/path'
+kitty='kitty --single-instance --wait-for-single-instance-window-close'
+nix_config='$('$find' "*.nix" | '$fzf'  | '$tee')'
+notes='$('$find' "*.md" | '$fzf'  | '$tee')'
+scripts='$('$find' "*.sh" | '$fzf'  | '$tee')'
+conf_options="Nixconfig\nScripts\nNotes\nExit"
+rebuild_options="Yes\nNo"
+dmenu='dmenu -sync -i -p'
 n=4
-config() {
+
+
+index_options() {
 
 	case "$1" in 
 		"")
 		;;
 	Nixconfig)
-		  kitty --class=ytfzf -e sh -c "pushd /persist/home/aruna; find nixconfig/ -name '*.nix' | fzf --preview=\"pistol {}\" | tee /tmp/path | xargs vi; popd"
-		;;
+                  $kitty $SHELL -c "vi $nix_config"
+	        ;;
 	Scripts)
-		 kitty --class=ytfzf -e sh -c "pushd /persist/home/aruna; find nixconfig/config/scripts -name '*.sh' | fzf --preview=\"pistol {}\" | tee /tmp/path | xargs vi; popd"
+		  $kitty $SHELL -c "vi $scripts"
 		;;
 	Notes)
-		 kitty --class=ytfzf -e sh -c "pushd /persist/home/aruna; find nixconfig -name '*.md' | fzf --preview=\"pistol {}\" | tee /tmp/path | xargs vi; popd" 
-		 ;;
+		  $kitty $SHELL -c "vi $notes"
+	        ;;
 	Exit) 
 		n=5
 esac
 
 }
 
-OPTIONS="Nixconfig\nScripts\nNotes\nExit"
 
-conf() { 
-	config "$(printf "%b" "$OPTIONS" | dmenu -i -l 4 -p "Config")";
+# rebuild
+rebuild_menu() {
+	case "$1" in 
+		"")
+			;;
+	Yes)
+		$kitty --hold $SHELL -c "cd /persist/home/aruna/nixconfig; doas nixos-rebuild switch --flake .#laptop"
+		nix_rebuild
+
+	;;
+No)
+	notify-send "ok" 
+	rm -f /tmp/build_failed
+esac
 }
 
+
+index_menu() {
+	index_options "$(printf "%b" "$conf_options" | $dmenu "Config")";
+}
+
+rebuild_query() {
+     rebuild_menu "$(printf "%b" "$rebuild_options" | sort | $dmenu "Rebuild?")";
+}
+
+nix_rebuild() {
+echo $?
+if [ $? -ne 0 ];
+then touch /tmp/build_failed
+else 
+	rm -f /tmp/build_failed 
+fi
+}
+
+
+main() {
 while [ $n == 4 ]
 do
-	conf
+      index_menu
 done
 
-~/.config/scripts/rebuild.sh;
+rebuild_query
 
 if test -f /tmp/build_failed;
 then 
-	kitty --class=ytfzf -e sh -c "vi $(cat /tmp/path)"
-	~/.config/scripts/rebuild.sh
-fi 
+	$kitty $SHELL -c "vi $(cat /tmp/path)"
+	rebuild_query
+fi
+
+}
 
 
+main 
 
